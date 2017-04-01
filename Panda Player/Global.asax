2d7aa4b58b5e2c3b64847d3f23d562a660aa9988
A.Aleksandrov <a.aleksandrov91@gmail.com>
2017-04-01 17:10:35 +0300

    Uploaded empty ASP.Net project with changed password requirements.

---
+++ Panda Player/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Panda_Player.MvcApplication" Language="C#" %>
---
+++ Panda Player/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Panda_Player.Startup))]
+namespace Panda_Player
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
