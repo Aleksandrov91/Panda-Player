74fc8b26c034e1cbf733c32c0c8366a2395bd3cc
flyelmos <emo_55@abv.bg>
2017-04-11 20:01:25 +0300

    File _Layout.cshtml Upgraded. Navigation bar settled and base theme for home page updated. Some changes made to Index.cshtml view also.

--- Panda Player/App_Start/BundleConfig.cs
+++ Panda Player/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Panda_Player
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/try2.css"));
         }
     }
 }
