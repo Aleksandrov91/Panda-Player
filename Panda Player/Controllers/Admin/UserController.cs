4734907207848bc255532dd3cf4ddd6cf2b17b32
HristoSpasov <hts_arenabg@yahoo.com>
2017-04-13 21:29:29 +0300

    Fixed bug - admin panel user controller

---
+++ Panda Player/Controllers/Admin/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Panda_Player.Models;
+using Panda_Player.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Panda_Player.Controllers.Admin
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // GET: User/List
+        public ActionResult List()
+        {
+            var db = new ApplicationDbContext();
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            var userViewModels = db.Users.ToArray().Select(user => new UserDetailsViewModel()
+            {
+                Id = user.Id,
+                Email = user.Email,
+                FullName = user.FullName,
+                Roles = string.Join(", ", userManager.GetRoles(user.Id)),
+            })
+            .ToArray();
+
+            //var users = db.Users.Include(r => r.Roles).Select(user => db.Roles.Where(role => user.Roles.Select(a => a.RoleId).ToArray().Contains(role.Id)).Select(a => a.Name).ToArray()).ToArray();
+
+            return View(userViewModels);
+        }
+    }
+}
---
+++ Panda Player/Models/Identity/UserDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Panda_Player.Models.Identity
+{
+    public class UserDetailsViewModel
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Roles { get; set; }
+    }
+}
