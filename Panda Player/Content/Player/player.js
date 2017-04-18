5af004ee26490de4f3cc27b468a8eda99aebbfbe
A.Aleksandrov <a.aleksandrov91@gmail.com>
2017-04-18 21:39:10 +0300

    Bug fixes player autoplay

--- Panda Player/Content/Player/player.js
+++ Panda Player/Content/Player/player.js
@@ -4,4 +4,4 @@
     progressColor: 'purple',
 });
 
-wavesurfer.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+wavesurfer.load('/Uploads/Home.mp3');
