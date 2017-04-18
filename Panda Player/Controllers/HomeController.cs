157b7cdd07028f1bfabf3880ee31930151d3fe55
BauMc <krasiymihailov@gmail.com>
2017-04-18 19:02:28 +0300

    Update MySongs.cshtml. Writes Author and Title when delete song

--- Panda Player/Controllers/HomeController.cs
+++ Panda Player/Controllers/HomeController.cs
@@ -14,13 +14,14 @@ namespace Panda_Player.Controllers
             var lastSongs = db.Songs.OrderByDescending(s => s.UploadDate).Take(6).ToList();
 
             var playlists = db.Playlists.OrderByDescending(a => a.DateCreated)
-                .Where(p =>p.IsPublic)
+                .Where(p => p.IsPublic)
                 .Take(6)
                 .ToList();
 
+
             ViewBag.Songs = lastSongs;
             ViewBag.Playlists = playlists;
-            
+
             return View();
         }
     }
