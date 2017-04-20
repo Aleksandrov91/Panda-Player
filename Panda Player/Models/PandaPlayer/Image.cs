8bcd812320e5628659264125b4fc5cc77166e23c
flyelmos <emo_55@abv.bg>
2017-04-20 22:25:10 +0300

    Uploading Image.cs

---
+++ Panda Player/Models/PandaPlayer/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Panda_Player.Models.PandaPlayer
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string ImagePath { get; set; }
+
+    }
+}
