06809a635f987f85ba1106bcc448e88b9662904d
BauMc <krasiymihailov@gmail.com>
2017-04-25 20:41:40 +0300

    Bug fix

--- Panda Player/Controllers/PlaylistsController.cs
+++ Panda Player/Controllers/PlaylistsController.cs
@@ -207,55 +207,7 @@ namespace Panda_Player.Controllers
             this.AddNotification("The Playlist has been deleted successfully.", NotificationType.SUCCESS);
             return Json(new { Success = true });
         }
-
-        //[Authorize]
-        //public ActionResult LoadPlaylist(int? id)
-        //{
-        //    var playlist = db.Playlists.Include(song => song.Songs).FirstOrDefault(a => a.Id == id);
-        //    var playlistSongs = playlist.Songs.ToList();
-        //    var playlistName = playlist.PlaylistName;            
-
-        //    var result = new StringBuilder();
-
-        //    result.AppendLine("#EXTM3U");
-        //    result.AppendLine("");
-
-        //    foreach (var song in playlistSongs)
-        //    {
-        //        var formattedSong = $"#EXTINF:1,{song.Artist} - {song.Title}";
-        //        var songPath = song.SongPath;
-
-        //        result.AppendLine(formattedSong);
-        //        result.AppendLine($"http://localhost:4522{songPath}");
-        //    }
-
-        //    var directoryPath = HttpContext.Server.MapPath("~/Uploads/Playlists");
-
-        //    if (!Directory.Exists(directoryPath))
-        //    {
-        //        Directory.CreateDirectory(directoryPath);
-        //    }
-
-        //    string uploadDir = Server.MapPath("~/Uploads/Playlists/");
-        //    var myPlayList = $@"{uploadDir}currentPlaylist.m3u";
-
-        //    if (!System.IO.File.Exists($"{myPlayList}"))
-        //    {
-        //        System.IO.File.Create($"{myPlayList}");
-        //    }
-
-        //    if (!System.IO.File.Exists($"{myPlayList}"))
-        //    {
-        //        System.IO.File.Create($"{myPlayList}");
-        //    }
-                        
-
-        //    System.IO.File.WriteAllText(myPlayList, result.ToString());
-            
-
-        //    return null;
-        //}      
-
+        
         public void ConvertToM3u(Playlist playlist)
         {
             var playlistSongs = playlist.Songs.ToList();
